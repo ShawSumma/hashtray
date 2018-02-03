b88b4a72a84ae94fe45c3c94c8e6ffd2d102ade6
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-02-03 00:34:16 -0500

    elaborated the example;

--- 2tables.c
+++ 2tables.c
@@ -4,6 +4,7 @@ Partial-key cuckoo hash.
 Nik Sultana, University of Pennsylvania, February 2018
 */
 
+#include <assert.h>
 #include <stdint.h>
 
 #include "pchast_M100.h"
@@ -13,6 +14,18 @@ int
 main() {
   struct PCH_M100_table * tM = PCH_M100_create_table();
   struct PCH_S1000_table * tS = PCH_S1000_create_table();
+
+  PCH_M100_data_t dataM = 1;
+  PCH_M100_data_t metadataM = 1;
+  enum PCH_M100_outcome oM;
+  oM = PCH_M100_insert(tM, dataM, metadataM);
+  assert(PCH_M100_OK == oM);
+
+  PCH_S1000_data_t dataS = 2;
+  PCH_S1000_data_t metadataS = 2;
+  enum PCH_S1000_outcome oS;
+  oS = PCH_S1000_insert(tS, dataS, metadataS);
+  assert(PCH_S1000_OK == oM);
 
   PCH_M100_destroy_table(tM);
   PCH_S1000_destroy_table(tS);
